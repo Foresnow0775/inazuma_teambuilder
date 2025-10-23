e3f3be82ea0ada94949b69337b663e9a8897d086
Fore_snow <kokiomori0620@gmail.com>
2025-10-23 21:28:02 +0900

    Update infoimage.js

--- infoimage.js
+++ infoimage.js
@@ -9,32 +9,34 @@ document.querySelectorAll(".player-card").forEach(card => {
 
   // 画像ファイル名マッピング
   const positionMap = {
-    "GK": "https://foresnow0775.github.io/inazuma_teambuilder/images/information/0_1_GK.PNG",
-    "DF": "https://foresnow0775.github.io/inazuma_teambuilder/images/information/0_2_DF.PNG",
-    "MF": "https://foresnow0775.github.io/inazuma_teambuilder/images/information/0_3_MF.PNG",
-    "FW": "https://foresnow0775.github.io/inazuma_teambuilder/images/information/0_4_FW.PNG"
+    "GK": "https://foresnow0775.github.io/inazuma_teambuilder/images/0_1_GK.PNG",
+    "DF": "https://foresnow0775.github.io/inazuma_teambuilder/images/0_2_DF.PNG",
+    "MF": "https://foresnow0775.github.io/inazuma_teambuilder/images/0_3_MF.PNG",
+    "FW": "https://foresnow0775.github.io/inazuma_teambuilder/images/0_4_FW.PNG",
+    "FW": "https://foresnow0775.github.io/inazuma_teambuilder/images/0_5_unknown.PNG",
   };
 
   const elementMap = {
-    "wind":     "https://foresnow0775.github.io/inazuma_teambuilder/images/information/1_1_wind.PNG",
-    "forest":   "https://foresnow0775.github.io/inazuma_teambuilder/images/information/1_2_forest.PNG",
-    "fire":     "https://foresnow0775.github.io/inazuma_teambuilder/images/information/1_3_fire.PNG",
-    "mountain": "https://foresnow0775.github.io/inazuma_teambuilder/images/information/1_4_mountain.PNG",
+    "wind":     "https://foresnow0775.github.io/inazuma_teambuilder/images/1_1_wind.PNG",
+    "forest":   "https://foresnow0775.github.io/inazuma_teambuilder/images/1_2_forest.PNG",
+    "fire":     "https://foresnow0775.github.io/inazuma_teambuilder/images/1_3_fire.PNG",
+    "mountain": "https://foresnow0775.github.io/inazuma_teambuilder/images/1_4_mountain.PNG",
+    "unknown": "https://foresnow0775.github.io/inazuma_teambuilder/images/1_5_unknown.PNG",
   };
 
   const gradeMap = {
-    "1st":     "https://foresnow0775.github.io/inazuma_teambuilder/images/information/2_1_1st.PNG",
-    "2nd":     "https://foresnow0775.github.io/inazuma_teambuilder/images/information/2_2_2nd.PNG",
-    "3rd":     "https://foresnow0775.github.io/inazuma_teambuilder/images/information/2_3_3rd.PNG",
-    "adult":   "https://foresnow0775.github.io/inazuma_teambuilder/images/information/2_4_adult.PNG",
-    "child":   "https://foresnow0775.github.io/inazuma_teambuilder/images/information/2_5_child.PNG",
-    "unknown": "https://foresnow0775.github.io/inazuma_teambuilder/images/information/2_6_unknown.PNG",
+    "1st":     "https://foresnow0775.github.io/inazuma_teambuilder/images/2_1_1st.PNG",
+    "2nd":     "https://foresnow0775.github.io/inazuma_teambuilder/images/2_2_2nd.PNG",
+    "3rd":     "https://foresnow0775.github.io/inazuma_teambuilder/images/2_3_3rd.PNG",
+    "adult":   "https://foresnow0775.github.io/inazuma_teambuilder/images/2_4_adult.PNG",
+    "child":   "https://foresnow0775.github.io/inazuma_teambuilder/images/2_5_child.PNG",
+    "unknown": "https://foresnow0775.github.io/inazuma_teambuilder/images/2_6_unknown.PNG",
   };
 
   const genderMap = {
-    "male":    "https://foresnow0775.github.io/inazuma_teambuilder/images/information/3_1_male.PNG",
-    "female":  "https://foresnow0775.github.io/inazuma_teambuilder/images/information/3_2_female.PNG",
-    "unknown": "https://foresnow0775.github.io/inazuma_teambuilder/images/information/3_3_unknown.PNG",
+    "male":    "https://foresnow0775.github.io/inazuma_teambuilder/images/3_1_male.PNG",
+    "female":  "https://foresnow0775.github.io/inazuma_teambuilder/images/3_2_female.PNG",
+    "unknown": "https://foresnow0775.github.io/inazuma_teambuilder/images/3_3_unknown.PNG",
   };
 
   // パスを決定
